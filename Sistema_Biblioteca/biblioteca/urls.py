23935530196410105fabb6b8c66d1db5f5b75b12
Bianka Ibrahim <biankaibrahim55@gmail.com>
2025-11-05 20:53:26 -0300

    Implementacao de Visualizar livro disponivil, adicionar no carrinho e finaizar compra

--- Sistema_Biblioteca/biblioteca/urls.py
+++ Sistema_Biblioteca/biblioteca/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('livros/<str:categoria>/', views.lista_livros, name='telaLivros'),
     path('livro/<int:id>/', views.detalhes_livro, name='detalhesLivro'),
     path('contato', views.telaContato, name='telaContato'),
-
+    path('pedido', views.pedido, name='telaPedido'),
 ]
--- Sistema_Biblioteca/biblioteca/views.py
+++ Sistema_Biblioteca/biblioteca/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
 from .models import Livro
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 
 def telaInicial(request):
@@ -26,3 +28,27 @@ def detalhes_livro(request, id):
 
 def telaContato(request):
     return render(request, "biblioteca/telaContato.html")
+
+def pedido(request):
+    livro_id = request.GET.get('livro_id')
+    quantidade = int(request.GET.get('quantidade', 1))
+
+    livro = None
+    if livro_id:
+        livro = get_object_or_404(Livro, id=livro_id)
+
+    if 'aumentar' in request.GET:
+        quantidade += 1
+    elif 'diminuir' in request.GET and quantidade > 1:
+        quantidade -= 1
+
+    if 'finalizar' in request.GET:
+        if livro:
+            messages.success(request, 'Pedido realizado com sucesso!')
+        else:
+            messages.warning(request, 'Nenhum livro selecionado.')
+
+    return render(request, 'biblioteca/pedido.html', {
+        'livro': livro,
+        'quantidade': quantidade
+    })
